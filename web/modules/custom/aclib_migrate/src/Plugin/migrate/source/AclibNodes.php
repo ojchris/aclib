28f65e4edca8aa22a6b123e8ab3bba2ed236dcfe
Michael Anello <manello@gmail.com>
2021-07-26 12:48:33 +0200

    Initial work on blog post migration (OA5736).

---
+++ web/modules/custom/aclib_migrate/src/Plugin/migrate/source/AclibNodes.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\aclib_migrate\Plugin\migrate\source;
+
+use Drupal\node\Plugin\migrate\source\d7\Node;
+use Drupal\migrate\Row;
+
+/**
+ * Custom ACLIB Drupal 7 node source from database.
+ *
+ * Includes URL aliases, limits nodes by date.
+ *
+ * Available configuration keys:
+ * - node_type: The node_types to get from the source - can be a string or
+ *   an array. If not declared then nodes of all types will be retrieved.
+ *
+ * @MigrateSource(
+ *   id = "aclib_d7_node",
+ *   source_module = "node"
+ * )
+ */
+class AclibNodes extends Node {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+
+    // Only migrate nodes created since 1/1/2020.
+    $query->condition('n.created', 1577833200, '>=');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // Include path alias.
+    $nid = $row->getSourceProperty('nid');
+    $query = $this->select('url_alias', 'ua')
+      ->fields('ua', ['alias']);
+    $query->condition('ua.source', 'node/' . $nid);
+    $alias = $query->execute()->fetchField();
+    if (!empty($alias)) {
+      $row->setSourceProperty('alias', '/' . $alias);
+    }
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return ['alias' => $this->t('Path alias')] + parent::fields();
+  }
+
+}
