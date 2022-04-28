b820763135aa85c91c3ce47d698c7d482808004a
nk- <nenadkesic@gmail.com>
2022-04-28 16:55:30 +0200

    Mobile logic (collapsible) for mega menu.

--- web/themes/custom/aclib9/js/custom.js
+++ web/themes/custom/aclib9/js/custom.js
@@ -14,5 +14,48 @@
 
     }
   };
+  
+  Drupal.behaviors.aclibTbMegaMenu = {
+  
+    attach: function(context, settings) {
+	
+	  var self = this;
+	  
+	  $('.dropdown-toggle.tb-megamenu-no-link', context).each(function(index, element) {
+	  
+	    var submenu = $(element).next();
+	    if (submenu.length && submenu.hasClass('tb-megamenu-submenu')) {
+		  // Do action only for screens with burger menu.
+	      if ($(window).width() < 980) {
+		    self.toggleCollapsed($(element), submenu, index, context);
+          }
+          else {
+	        $(element).removeAttr('role');
+	        submenu.removeClass('visually-hidden');
+	      }
+        }
+      });
+    },
+    
+    toggleCollapsed: function(element, submenu, index, context) {
+	  var self = this;
+	  element.attr('role', 'button');
+	  submenu.addClass('visually-hidden');
+	  element.on('click', function(e) {
+        submenu.toggleClass('visually-hidden');
+        self.mapSiblings($(e.target).parent().siblings(), index, context);
+      });
+	},
+	
+	mapSiblings: function(siblings, index, context) {
+   	  siblings.each(function(index, sibling) {
+	    var siblingChild = $(sibling).find('.tb-megamenu-submenu');
+	    if (siblingChild.length) {
+		  siblingChild.addClass('visually-hidden');
+	    } 
+	  });
+    }
+  
+  };
 
 })(jQuery, Drupal);
