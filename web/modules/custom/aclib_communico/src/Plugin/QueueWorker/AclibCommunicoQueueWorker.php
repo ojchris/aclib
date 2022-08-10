843181e9e47d490f2e68cc0659d28f5dc7e85588
Michael Anello <michael@anelloconsulting.com>
2022-08-10 11:51:42 +0200

    Update to PHP 8.1 initial testing (OA5893).

--- web/modules/custom/aclib_communico/src/Plugin/QueueWorker/AclibCommunicoQueueWorker.php
+++ web/modules/custom/aclib_communico/src/Plugin/QueueWorker/AclibCommunicoQueueWorker.php
@@ -30,7 +30,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
   /**
    * Drupal\Core\StringTranslation\StringTranslationTrait definition.
    * Wrapper methods for \Drupal\Core\StringTranslation\TranslationInterface.
-   * 
+   *
    * @var \Drupal\Core\StringTranslation\StringTranslationTrait
    */
   use StringTranslationTrait;
@@ -44,7 +44,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
 
   /**
    * Fields mapping
-   * 
+   *
    * communico_response_field_name => drupal_field_name
   */
   const FIELDS_MAP = [
@@ -74,7 +74,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
 
   /**
    * Drupal\Core\Config\ConfigFactoryInterface definition.
-   * 
+   *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $configFactory;
@@ -85,14 +85,14 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
-  
+
   /**
    * Drupal\Core\Logger\LoggerChannelFactoryInterface definition.
    *
    * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
    */
   protected $logger;
-  
+
   /**
    * {@inheritdoc}
    */
@@ -102,7 +102,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
     $this->entityTypeManager = $entity_type_manager;
     $this->logger = $logger;
   }
-  
+
   /**
    * {@inheritdoc}
    */
@@ -121,18 +121,18 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
    * {@inheritdoc}
    */
   public function processItem($data) {
-  
+
     // Load our configuration object
     $config = $this->configFactory->get('aclib_communico.settings');
-  
+
     // Prepare our data for to be associative array ready for node save
     $node_data = $this->prepareItem($data, $config);
 
-    // Find possible existing equivalent communico event in our (Drupal) database 
+    // Find possible existing equivalent communico event in our (Drupal) database
     $properties = [
       'field_communico_event_id' => $node_data['field_communico_event_id']
     ];
-  
+
     $communico_event_load = $this->entityTypeManager->getStorage('node')->loadByProperties($properties);
     $communico_event = is_array($communico_event_load) && !empty($communico_event_load) ? reset($communico_event_load) : NULL;
 
@@ -143,7 +143,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
     }
     // Yet this is a communico event that we did not import so far
     else {
-      $this->saveItem($node_data, 'create'); 
+      $this->saveItem($node_data, 'create');
     }
   }
 
@@ -169,20 +169,26 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
     // Fetch default timezone from the main Drupal's configuration at "/admin/config/regional/settings"
     $default_timezone = $this->configFactory->get('system.date')->get('timezone');
     $timezone = isset($default_timezone['default']) && !empty($default_timezone['default']) ? $default_timezone['default'] : static::DEFAULT_TIMEZONE;
-    
+
     foreach ($data as $field_key => $field) {
       if (in_array($field_key, array_keys($fields_map))) {
         $drupal_field_name = $fields_map[$field_key];
         if ($drupal_field_name) {
-          
-          // Dates need a special care, we need to assign our timezone 
+
+          // Dates need a special care, we need to assign our timezone
           if ($drupal_field_name == 'field_start_date' || $drupal_field_name == 'field_end_date') {
             $field = $this->prepareDates($drupal_field_name, $node_data['type'], $field, $timezone);
           }
-          
+
           // Strip html tags on this field
           if ($drupal_field_name == 'field_external_description') {
-            $field = trim(strip_tags($field));
+            if (!is_null($field)) {
+              $field = trim(strip_tags($field));
+              if (strlen($field > 250)) {
+                // Trim this field in case it is too long.
+                preg_replace('/\s+?(\S+)?$/', '', substr($field, 0, 250));
+              }
+            }
           }
 
           // Checkbox needs extra handling
@@ -192,7 +198,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
 
           $node_data[$drupal_field_name] = $field;
         }
-      }    
+      }
     }
 
     ksort($node_data); // Essential for matching with local/drupal fields values
@@ -210,11 +216,11 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
    *   Node entity object.
    */
   protected function saveItem(array $data, string $op, object $communico_event = NULL) {
-  
+
     switch ($op) {
-    
+
       case 'update':
-        
+
         try {
 
           $remote_values_hash = Crypt::hashBase64(implode('__', array_values($data)));
@@ -226,7 +232,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
             $warning = $this->t('Communico hash field not existing.');
             $this->logger->get('aclib_communico')->warning($warning);
           }
-          
+
           if ($remote_values_hash != $drupal_values_hash) {
             foreach ($data as $field_name => $field_value) {
               if ($field_name !== 'type' && $field_name !== 'uid') {
@@ -234,14 +240,14 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
                 $communico_event->set($field_name, $value);
               }
             }
-     
+
             // Finally save updates on our node object
             if ($communico_event->save()) {
               $status = $this->t('Existing node updated with a new version imported via Communico API: @title ', ['@title' => $data['title']]);
               $this->logger->get('aclib_communico')->notice($status);
             }
           }
-          
+
         }
         catch (\Exception $e) {
           $error = $this->t('Updating Communico event nodes failed on cron run for event: @title with message: @message', ['@title' => $data['title'], '@message' => $e->getMessage()]);
@@ -252,7 +258,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
       case 'create':
 
         try {
-       
+
           $node = $this->entityTypeManager->getStorage('node')->create($data);
           if ($node->save()) {
             $status = $this->t('A new node imported via Communico API; @title', ['@title' => $data['title']]);
@@ -264,9 +270,9 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
           $this->logger->get('aclib_communico')->error($error);
         }
       break;
-     
+
     }
-  } 
+  }
 
   /**
    * Operation callback, prepare dates and convert into UTC string to be saved in database
@@ -285,9 +291,9 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
    *   Formatted date string with time converted to UTC
    */
   protected function prepareDates(string $field_name, string $bundle, string $field_value = 'now', string $timezone = '') {
-  
+
     $date_field_storage = $this->entityTypeManager->getStorage('entity_view_display')->load('node.' . $bundle . '.default');
-    
+
     // First check if there is timezone override on Manage display for a field
     // Fallback to default timezone settings in Drupal at "/admin/config/regional/settings"
     if (is_object($date_field_storage) && !empty($date_field_storage->getRenderer($field_name)->getSettings())) {
@@ -316,7 +322,7 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
    *   Based on this value we find mathing node in Drupal (does not exist anymore on Communico), for unpublishing
    */
   public static function matchRequestFilters(object $config, NodeInterface $node) {
-  
+
     $config_start_date = NULL;
     $config_end_date = NULL;
     $start_date = NULL;
@@ -331,49 +337,49 @@ class AclibCommunicoQueueWorker extends QueueWorkerBase implements ContainerFact
     if ($config->get('startDate')) {
       /* @var \Drupal\Core\Datetime\DrupalDateTime */
       $config_start_date = new DrupalDateTime($config->get('startDate'));
-      // Does node have a field_start_date field value? 
+      // Does node have a field_start_date field value?
       if ($node->hasField('field_start_date') && !empty($node->get('field_start_date')->getValue()) && !empty($node->get('field_start_date')->getValue()[0]['value'])) {
         /* @var \Drupal\Core\Datetime\DrupalDateTime */
         $start_date = new DrupalDateTime($node->get('field_start_date')->getValue()[0]['value']);
-      } 
+      }
     }
-  
+
     // Is endDate set as API call parameter on configuration?
     // If yes prepare date objects for matching
     if ($config->get('endDate')) {
       /* @var \Drupal\Core\Datetime\DrupalDateTime */
       $config_end_date = new DrupalDateTime($config->get('endDate'));
-      // Does node have a field_end_date field value? 
+      // Does node have a field_end_date field value?
       if ($node->hasField('field_end_date') && !empty($node->get('field_end_date')->getValue()) && !empty($node->get('field_end_date')->getValue()[0]['value'])) {
         /* @var \Drupal\Core\Datetime\DrupalDateTime */
         $end_date = new DrupalDateTime($node->get('field_end_date')->getValue()[0]['value']);
-      } 
+      }
     }
-  
+
     // Are events types set as API call parameter on configuration?
     // If yes prepare hash strings for matching
     if (is_array($config->get('types')) && !empty($config->get('types'))) {
       $config_types = Crypt::hashBase64(implode('__', array_values($config->get('types'))));
       $events_types = !empty($node->get('communico_events_type')->getValue()) && !empty($node->get('communico_events_type')->getValue()[0]['value']) ? $node->get('communico_events_type')->getValue()[0]['value'] : '';
     }
-  
+
     if ($config_types) {
       if ($events_types && $config_types == $events_types) { // Hashed values of Events type set on config matches with those on node ("communico_events_type" computed field)
-        $match = 'events'; 
-      } 
+        $match = 'events';
+      }
     }
 
     if ($config_start_date && $config_end_date) {
-      if ($start_date && $end_date) { 
+      if ($start_date && $end_date) {
         $match = $start_date >= $config_start_date && $end_date <= $config_end_date ? 'dates' : FALSE;
       }
       else {
         $match = $match == 'events' ? TRUE : FALSE; // We do not have both dates set on node so it does not match with communico filters and - we should not unpublish
-      } 
+      }
     }
     else {
       $match = $match != 'events' ? TRUE : FALSE; // No filters by start and end date set on configuration for API call - we should unpublish if events filter matches too
-    } 
+    }
 
     return $match;
   }
